03859ddd3ffe6083ffebd627ff318c26063e0504
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-11-01 17:48:13 -0600

    Showing the result of the word encrypt and desenceypt

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 let $inputText = document.querySelector('[data-text]');
 const $encryptButton = document.querySelector('[data-encrypt]');
+const $desencryptButton = document.querySelector('[data-desencrypt]');
+const $showMessage = document.querySelector('[data-show]');
+const span = document.createElement("span");
+
 
 $encryptButton.addEventListener("click", (e) => {
+    showResult(e, $inputText);
+});
 
-    console.log(encryptWord($inputText))
+$desencryptButton.addEventListener("click", (e) => {
+    showResult(e, $inputText);
 });
 
 const encryptWord = ($inputText) => {
@@ -15,4 +22,33 @@ const encryptWord = ($inputText) => {
     encryptWord = encryptWord.replaceAll(/u/g, "ufat");
 
     return encryptWord;
+}
+
+const desencryptWord = ($inputText) => {
+    let encryptWord = $inputText.value;
+    encryptWord = encryptWord.replaceAll(/enter/g, "e");
+    encryptWord = encryptWord.replaceAll(/imes/g, "i");
+    encryptWord = encryptWord.replaceAll(/ai/g, "a");
+    encryptWord = encryptWord.replaceAll(/ober/g, "o");
+    encryptWord = encryptWord.replaceAll(/ufat/g, "u");
+
+    return encryptWord;
+}
+
+const showResult = (e, input) => {
+    const buttonSelected = e.target.dataset.button;
+    let word = "";
+
+    switch (buttonSelected) {
+        case "encrypt":
+            word = encryptWord(input);
+            break;
+
+        default:
+            word = desencryptWord(input);
+            break;
+    }
+    $showMessage.innerHTML=""
+    span.innerText = word;
+    $showMessage.appendChild(span);
 }
